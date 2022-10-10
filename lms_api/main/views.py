ad53293e9c3fc60cd612812de16e778208d1e35a
Tooni <prashanth04@gmail.com>
2022-10-11 00:06:03 +0530

    Authentication and Permissions

--- lms_api/main/views.py
+++ lms_api/main/views.py
@@ -3,6 +3,7 @@ from rest_framework.views import APIView
 from .serializers import TeacherSerializer
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework import permissions
 from .models import (Teacher, Course, CourseCategory, Student)
 # Create your views here.
 
@@ -18,8 +19,10 @@ from .models import (Teacher, Course, CourseCategory, Student)
 class TeacherList(generics.ListCreateAPIView):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
